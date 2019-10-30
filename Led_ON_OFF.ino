36dcae1f24feacf721e45dd06defbdffac713472
eng-abdulhanan <57168597+eng-abdulhanan@users.noreply.github.com>
2019-10-31 01:03:10 +0500

    Add files via upload

---
+++ Led_ON_OFF.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+
+  pinMode(2,INPUT_PULLUP);
+  pinMode(13,OUTPUT);
+}
+void loop()
+{
+  int val =digitalRead(2);
+  if(val==LOW)
+    {
+      digitalWrite(13,HIGH);
+    }
+  else
+  {
+      digitalWrite(13,LOW);
+  }
+}
